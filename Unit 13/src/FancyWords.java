1eab1a40e07c90959ad6c0b43b53dad70c82a1ea
shanewaxler <shanewaxler1@gmail.com>
2018-04-16 11:05:44 -0700

    blop

---
+++ Unit 13/src/FancyWords.java
@@ -0,0 +1,41 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class FancyWords
+{
+	private String[] wordRay;
+	private String[] newWord;
+
+	public FancyWords(String sentence, int num)
+	{
+		setWords(sentence, num);
+	}
+
+	public void setWords(String sentence, int num)
+	{
+		for (int i = 0; i<num;i++){
+			
+		}
+	}
+
+	public String toString()
+	{
+		String output="";
+		int max = Integer.MIN_VALUE;
+
+
+
+
+
+
+		return output+"\n\n";
+	}
+}
---
+++ Unit 15/src/Paddle.java
@@ -0,0 +1,78 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle extends Block
+{
+	private int speed;
+
+	public Paddle()
+	{
+		super(10,10);
+		setSpeed(5);
+	}
+
+	public Paddle(int x, int y) {
+		super(x, y);
+		setSpeed(5);
+	}
+	
+	public Paddle(int x, int y, int s) {
+		super(x, y);
+		setSpeed(s);
+	}
+	
+	public Paddle(int x, int y, int w, int h, int s) {
+		super(x, y, w, h);
+		setSpeed(s);
+	}
+	
+	public Paddle(int x, int y, int w, int h, Color col, int s) {
+		super(x, y, w, h, col);
+		setSpeed(s);
+	}
+
+
+	//SET METHODS
+	public void setSpeed(int s) {
+		speed = s;
+	}
+	
+	//GET METHODS
+	public int getSpeed() {
+		return speed;
+	}
+
+	//DRAWING
+	public void moveUpAndDraw(Graphics window)
+	{
+		Color temp = getColor();
+		draw(window, Color.WHITE);
+		setY(getY()-getSpeed());
+		draw(window, temp);
+	}
+	
+	public void moveDownAndDraw(Graphics window)
+	{
+		Color temp = getColor();
+		draw(window, Color.WHITE);
+		setY(getY()+getSpeed());
+		draw(window, temp);
+	}
+	
+	public boolean equals(Object obj)
+	{
+   		Paddle other = (Paddle) obj;
+   		return super.equals(other) 
+				&& this.getSpeed() == other.getSpeed();	
+	}
+	   
+	public String toString() {
+		return super.toString() +" "+ getSpeed();
+	}
+}
